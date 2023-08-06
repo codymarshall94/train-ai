d244b6d394f57b3f4649478b051e421788014ebf
Cody Marshall <codymstuntsx@gmail.com>
2023-08-05 22:31:09 -0700

    UPDATED: pathname added for styles

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { Montserrat } from "next/font/google";
 import { Brain, LayoutDashboard, Settings } from "lucide-react";
 
@@ -27,10 +28,11 @@ const routes = [
     label: "Settings",
     icon: Settings,
     href: "/settings",
-  }
+  },
 ];
 
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -47,7 +49,12 @@ export default function Sidebar() {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
