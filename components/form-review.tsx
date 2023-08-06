fa8e92237d14ecfb5f073138ef3d53b816bfcd3d
Cody Marshall <codymstuntsx@gmail.com>
2023-08-05 23:19:49 -0700

    REFACTORED: added items to array to be mapped over

--- components/form-review.tsx
+++ components/form-review.tsx
@@ -2,27 +2,35 @@ import React from "react";
 
 export default function FormReview({ form }: Record<string, any>) {
   const { goal, experience, days, equipment, sport, length, injuries } = form;
+  const itemsToReview = [
+    { name: "Goal", value: goal },
+    { name: "Experience", value: experience },
+    { name: "Days", value: days },
+    { name: "Equipment", value: equipment.join(", ") },
+    { name: "Sport", value: sport },
+    { name: "Length", value: length },
+    { name: "Injuries", value: injuries.join(", ") },
+  ];
+
   return (
     <div className="px-4 lg:px-8 mb-8">
-      <h2 className="text-2xl font-bold text-center my-8">
+      <h2 className="text-2xl font-bold text-center my-2">
         Does this look correct?
       </h2>
+      <p className="text-sm text-muted-foreground font-light text-center mb-6">
+        If not, click back to change your answers. Otherwise, click generate to
+        create your plan.
+      </p>
       <div className="flex flex-col space-y-4">
         <div className="flex flex-col space-y-2">
-          <h3 className="text-sm text-muted-foreground font-light">Goal</h3>
-          <p className="capitalize">{goal}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Experience</h3>
-          <p className="capitalize">{experience}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Days</h3>
-          <p className="capitalize">{days}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Equipment</h3>
-          <p className="capitalize">{equipment.join(", ")}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Sport</h3>
-          <p className="capitalize">{sport}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Length</h3>
-          <p className="capitalize">{length}</p>
-          <h3 className="text-sm text-muted-foreground font-light">Injuries</h3>
-          <p className="capitalize">{injuries.join(", ")}</p>
+          {itemsToReview.map((item) => (
+            <div key={item.name}>
+              <p className="text-sm text-muted-foreground font-light">
+                {item.name}
+              </p>
+              <p className="text-lg font-bold">{item.value}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
