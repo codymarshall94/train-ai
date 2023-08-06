6cc56a72aba4e3329c33928d1f2452c6c58b0505
Cody Marshall <codymstuntsx@gmail.com>
2023-08-05 23:20:32 -0700

    UPDATED: loading component to show pink logo

--- components/loading.tsx
+++ components/loading.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+    <div className="h-full flex flex-col py-4 gap-y-4 items-center justify-center">
       <div className="w-10 h-10 relative animate-spin">
-        <Image alt="Logo" src="/logo.png" fill />
+        <Image alt="Logo" src="/logoPink.png" fill />
       </div>
-      <p className="text-sm text-muted-foreground">Building your plan...</p>
+      <p className="text-sm text-muted-foreground">Working out your plan...</p>
     </div>
   );
 }
