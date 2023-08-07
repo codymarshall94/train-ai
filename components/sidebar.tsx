0a375ed7defe9df67bc176add634594ffa457358
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 15:51:58 -0700

    UPDATED: logo color

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -38,10 +38,10 @@ export default function Sidebar() {
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
-            <Image src="/logo.png" fill alt="Logo" />
+            <Image src="/logoPink.png" fill alt="Logo" />
           </div>
           <h1 className={cn("text-2xl font-bold", monstserrat.className)}>
-            trAIn
+            tr<span className="text-[#B03170]">AI</span>n
           </h1>
         </Link>
         <div className="space-y-1">
