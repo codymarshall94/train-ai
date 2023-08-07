819dea1f26c0339878e9b2debb7cccde79a53097
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 15:51:47 -0700

    UPDATED: links and hero image

---
+++ components/landing-hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+export default function LandingHero() {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <div className="text-white font-bold py-36 text-center space-y-5">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+        <h1>Planning clients made easy.</h1>
+      </div>
+      <div className="text-sm md:text-xl font-light text-zinc-400">
+        Choose your options and generate a plan in a few clicks.
+      </div>
+      <div>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button className="md:text-lg p-4 md:p-6 rounded-full font-semibold bg-[#B03170] hover:bg-[#A02E66]">
+            Start Generating For Free
+          </Button>
+        </Link>
+      </div>
+      <div className="hidden md:block relative w-96 h-96 mx-auto">
+        <Image src="/home.svg" alt="Hero Image" style={{ objectFit: "contain" }} width={500} height={500}/>
+      </div>
+    </div>
+  );
+}
---
+++ components/landing-navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+const font = Montserrat({ weight: "600", subsets: ["latin"] });
+
+export default function LandingNavbar() {
+  const { isSignedIn } = useAuth();
+  return (
+    <nav className="p-4 bg-transparent flex items-center justify-between">
+      <Link href="/" className="flex items-center">
+        <div className="relative h-8 w-8 mr-4">
+          <Image fill alt="Logo" src="/logoPink.png" />
+        </div>
+        <h1 className={cn("text-2xl font-bold text-white", font.className)}>
+          tr<span className="text-[#B03170]">AI</span>n
+        </h1>
+      </Link>
+      <div className="flex items-center space-x-4">
+        {isSignedIn ? (
+          <Link href="/dashboard">
+            <Button className="text-sm md:text-lg p-4 md:p-6 rounded-full font-semibold bg-[#B03170] hover:bg-[#A02E66]">
+              Dashboard
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button className="text-sm md:text-lg p-4 md:p-6 rounded-full font-semibold">
+              Login
+            </Button>
+            <Link href="/sign-up">
+              <Button className="text-sm md:text-lg p-4 md:p-6 rounded-full font-semibold bg-[#B03170] hover:bg-[#A02E66]">
+                Sign Up
+              </Button>
+            </Link>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+}
