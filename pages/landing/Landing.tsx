7bcb9ae64f3cf37c200d894929986cdb188a52be
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 15:52:51 -0700

    IMPORTED: landing hero

--- pages/landing/Landing.tsx
+++ pages/landing/Landing.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LandingNavbar from "@/components/landing-navbar";
+import LandingHero from "@/components/landing-hero";
 
 export default function Landing() {
   return (
     <div className="h-full ">
       <LandingNavbar />
+      <LandingHero />
     </div>
   );
 }
