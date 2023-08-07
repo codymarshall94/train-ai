28f9914c70ea69bdacd12682cd94aa5f7267ad90
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 15:53:43 -0700

    UPDATED: setloading time to true permanannt

--- pages/generator/Generator.tsx
+++ pages/generator/Generator.tsx
@@ -13,9 +13,6 @@ export default function Generator() {
 
   const handleGeneratePlan = () => {
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 4000);
   };
 
   return (
