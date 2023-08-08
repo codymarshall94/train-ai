20a275806e327b4e14f5e6244ca11ff97163bb53
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 17:16:36 -0700

    wrapped auth layout with clerk provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,10 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <ClerkProvider>
+      <div className="flex items-center justify-center h-full">{children}</div>
+    </ClerkProvider>
   );
 };
 
