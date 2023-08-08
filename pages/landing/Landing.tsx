d330dd514bee2c6d84a63d097fec88ba5f426a34
Cody Marshall <codymstuntsx@gmail.com>
2023-08-08 10:38:16 -0700

    UPDATED: included landing section component

--- pages/landing/Landing.tsx
+++ pages/landing/Landing.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import LandingNavbar from "@/components/landing-navbar";
-import LandingHero from "@/components/landing-hero";
+import LandingNavbar from "@/components/landing/landing-navbar";
+import LandingHero from "@/components/landing/landing-hero";
+import LandingHowWorks from "@/components/landing/landing-how-works";
+import LandingSection from "@/components/landing/landing-section";
+import LandingSectionHeader from "@/components/landing/landing-section-header";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Landing() {
   return (
     <div className="h-full ">
       <LandingNavbar />
       <LandingHero />
+      <LandingSection>
+        <LandingHowWorks />
+      </LandingSection>
+      <LandingSection>
+        <LandingSectionHeader
+          title="Ready to Simplify Your Training Process?"
+          subtitle="Start building today."
+        />
+        <div className="flex justify-center mt-8">
+          <Link href="/sign-in">
+            <Button>Get Started</Button>
+          </Link>
+        </div>
+      </LandingSection>
     </div>
   );
 }
