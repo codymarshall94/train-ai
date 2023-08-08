52f1c2701f6933aa6161a5636f6e56e419bc1c09
Cody Marshall <codymstuntsx@gmail.com>
2023-08-07 18:15:17 -0700

    UPDATED: clerk provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,9 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <div className="flex items-center justify-center h-full">{children}</div>
-    </ClerkProvider>
+    <div className="flex items-center justify-center h-full">{children}</div>
   );
 };
 
--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,15 +1,13 @@
-const LandingLayout = ({
-    children
-  }: {
-    children: React.ReactNode;
-  }) => {
-    return (
+import { ClerkProvider } from "@clerk/nextjs";
+
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
       <main className="h-full bg-[#111827] overflow-auto">
-        <div className="mx-auto max-w-screen-xl h-full w-full">
-          {children}
-        </div>
+        <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
       </main>
-     );
-  }
-   
-  export default LandingLayout;
+    </ClerkProvider>
+  );
+};
+
+export default LandingLayout;
