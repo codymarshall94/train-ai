6ba268e0d870d42998539f54a89e9469e376c4b2
Cody Marshall <codymstuntsx@gmail.com>
2023-08-11 16:03:20 -0700

    CREATED: helper functions for building out api cal

---
+++ lib/api-limit.ts
@@ -0,0 +1,53 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNTS } from "@/constants/constants";
+
+//We will use this function to increase the API limit of the user
+//This function will be called when the user has successfully created a new post
+//The userApiLimit will come from the model in @/prisma/prisma-schema
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId: userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: { userId: userId, count: 1 },
+    });
+  }
+};
+
+export const checkApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return false;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
+    return true;
+  } else {
+    return false;
+  }
+};
